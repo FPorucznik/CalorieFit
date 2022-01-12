c1651fffaacbbdeeb02e287b48e0cd858de56574
FPorucznik <porucznik199@wp.pl>
2022-01-12 16:10:34 +0100

    added dependencies and created entity model

---
+++ CalorieFit/app/src/main/java/com/example/caloriefit/model/FoodEntity.java
@@ -0,0 +1,41 @@
+package com.example.caloriefit.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "food_table")
+public class FoodEntity {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "food_name")
+    private String mName;
+
+    @NonNull
+    @ColumnInfo(name = "calorie_value")
+    private int mCalories;
+
+    @NonNull
+    @ColumnInfo(name = "photo_uri")
+    private String mPhoto;
+
+    public FoodEntity(@NonNull String name, @NonNull int calories){
+        this.mName = name;
+        this.mCalories = calories;
+        this.mPhoto = "";
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public int getCalories() {
+        return this.mCalories;
+    }
+
+    public String getPhoto() {
+        return this.mPhoto;
+    }
+}
